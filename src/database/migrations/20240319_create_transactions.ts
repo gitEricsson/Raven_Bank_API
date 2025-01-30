aef0a7fd2ed5eef21bca3648cdc9f3184fe9f630
Ericsson Raphael <ericssonraphael@gmail.com>
2025-01-30 17:57:39 +0100

    Update: Separated enums and interfaces

--- src/database/migrations/20240319_create_transactions.ts
+++ src/database/migrations/20240319_create_transactions.ts
@@ -1,5 +1,5 @@
 import { Knex } from 'knex';
-import { TransactionStatus, TransactionType } from 'src/types/enums';
+import { TransactionStatus, TransactionType } from '../../types/enums';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('transactions', (table) => {
